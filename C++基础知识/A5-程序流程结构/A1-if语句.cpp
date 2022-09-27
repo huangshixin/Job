25f1afc6642e1af81487016021ccc1ebf93bac63
huangshixin <38878365+huangshixin@users.noreply.github.com>
2022-09-27 16:54:31 +0800

    Update A1-if语句.cpp

--- C++基础知识/A5-程序流程结构/A1-if语句.cpp
+++ C++基础知识/A5-程序流程结构/A1-if语句.cpp
@@ -1,1 +1,55 @@
+顺序结构
+
+1、从头到尾的执行语句
+
+
+
+
+
+
+
+
+选择结构
+
+【单条if】
+   根据条件的符合程度，选择是否执行
+   //if条件后面，不要加上封号
+   	if (a > 20) {
+		cout << "ddddddddddd" << endl;
+	}
+
+
+【if-else】
+	if (a > 35) {
+		cout << "ddddddddddd" << endl;
+	}
+	else
+	{
+		cout << "bbbbb" << endl;
+	}
+
+
+【多条件语句】
+	int a =30;
+	int b=4;
+	if (a > 35) {
+		cout << "ddddddddddd" << endl;
+	}
+	else if (b<3)
+	{
+		cout << "bbbbb" << endl;
+	}
+	else {
+		cout << "请输入整数型数据a:" << a % b << endl;
+	}
+
+
+
+
+
+
+
+循环结构
+
+
 

7af408e6614e8a72a493ec46f8fd58c5f73c6de6
huangshixin <38878365+huangshixin@users.noreply.github.com>
2022-09-27 15:32:08 +0800

    Create A5-布尔和数据的输入.cpp

---
+++ C++基础知识/A3-数据类型/A5-布尔和数据的输入.cpp
@@ -0,0 +1,32 @@
+布尔类型
+
+1、只有两种 true 和 false
+  bool a = True
+  bool b = False
+【打印的时候是 0：代表false  1：代表true  】
+
+
+
+2、数据的输入
+
+cin
+
+
+【tips】 在给定输入的数据的时候，在系统中必须存在该变量
+
+【模板】
+	int a = 0;
+	cout << "请输入整数型数据a:" << endl;
+	cin >> a;
+	cout << "输出整数型数据a:" << a << endl;
+
+【tips2】在引用string的时候 需要包含“头文件”
+#include <string>
+
+
+【tips3】布尔类型 需要给的是 0 和 非0值（在控制台）
+非0值 都表示的是真
+
+
+
+

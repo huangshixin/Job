9c319b804414157212540a4bdbd0ba3ac60027f1
huangshixin <38878365+huangshixin@users.noreply.github.com>
2022-10-07 11:02:41 +0800

    Create A2-函数的常见央视.cpp

---
+++ C++基础知识/A7-函数/A2-函数的常见央视.cpp
@@ -0,0 +1,29 @@
+1、无参无返
+
+ viod function(){
+    cout<<"asda"<<endl;
+ }
+ 
+
+2、有参无返
+
+  viod function1(int num1){
+        cout<<"num1="<<num1<<endl;
+  }
+  
+
+3、无参有返
+     viod function3(){
+        return 'function3';
+     }
+
+4、有参有返
+
+    int function(int a){
+        return a;
+    }
+    
+
+
+
+5、函数的声明；

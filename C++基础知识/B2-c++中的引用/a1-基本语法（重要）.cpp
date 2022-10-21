74f2baa18f0474ebf38024f2c43d9e666cc2c9a4
huangshixin <38878365+huangshixin@users.noreply.github.com>
2022-10-21 16:14:57 +0800

    Update a1-基本语法（重要）.cpp

--- C++基础知识/B2-c++中的引用/a1-基本语法（重要）.cpp
+++ C++基础知识/B2-c++中的引用/a1-基本语法（重要）.cpp
@@ -83,6 +83,74 @@ int &test02{
 
 
 
+【左值：表示的是等号的左边】
+
+int &ref2 = test02();
+cout<<ref<<endl;//打印 10
+
+test02() = 1000;  设定‘引用’的函数可以作为左值，进行赋值；
+
+cout<<ref<<endl;//打印1000，注意是ref跟着改变了，因为它是test02的别名；
+
+
+
+
+【引用的本质】
+
+1、内部的实现就是一个【指针的常量】
+
+int * const ref=*a; 这个表示的是，指针的指向是固定的；但是你可以改值啊；
+
+
+【案例】
+int& ref=a;
+ref = 20;//内部发现ref是引用，自动帮我们转换为 *ref =20;,这里的ref可以直接访问的；
+//因为编译器帮你解引用了；
+
+本质：就是一个指针常量；
+
+
+
+
+
+【常量引用】
+
+使用场景：定义在函数中，防止误操作；
+
+int a=10
+int &b =a;  ---合法；
+
+
+但是：
+int a=10;
+int &b=10;不合法--因为你需要接受的是一个地址
+
+怎么处理呢？
+1、要么按照上面的那种方式
+2、要么变成
+int a=10;
+const int &b=10;加上const后，编译器将代码修改为 int temp=10;const int &ref =temp;
+
+[tips]不允许修改别名了
+
+
+
+//使用场景，修饰形参防止误操作
+
+void showValue(const int &val){
+
+    // val = 1000;//这个必须是可修改的左值
+    cout<<val<<endl;
+
+}
+int main(){
+
+  int a=10;
+  showValue(a);
+  system("pause");
+  return 0;
+}  
+
 
 
 
